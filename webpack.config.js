697886afecba303d413f25056992301833133beb
Chris Gonzalez <chris.gonzalez@turner.com>
2016-04-04 14:52:25 -0400

    Moving things around

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         main: [
-            path.join(__dirname, 'containers/index.js')
+            path.join(__dirname, 'src/containers/index.js')
         ]
     },
     output: {
-        filename: path.join(__dirname, 'dist/js/bundle.js'),
-        publicPath: '/dist'
+        filename: 'js/bundle.js',
+        path: path.join(__dirname, '/dist'),
+        publicPath: '/'
     },
     module: {
         loaders: [
@@ -28,18 +30,40 @@ module.exports = {
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass')
+                loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass'),
+                publicPath: '/'
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: 'html'
             }
         ]
     },
     plugins: [
         // Extract out the CSS into their own external files
-        new ExtractTextPlugin('dist/css/[name].css', {
-            disable: false
+        new ExtractTextPlugin('css/[name].css', {
+            disable: false,
+            path: path.join(__dirname, '/dist'),
+            publicPath: '/'
+        }),
+
+        // HTML output
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'),
+            inject: 'body',
+            filename: 'index.html',
+            // Minification options:
+            // https://github.com/kangax/html-minifier#options-quick-reference
+            minify: {
+                collapseWhitespace: true,
+                minifyJS: true,
+                removeComments: true
+            }
         })
     ],
     resolve: {
-        root: path.join(__dirname, 'components'),
+        root: path.join(__dirname, 'src/components'),
         extensions: [
             '',
             '.js',
